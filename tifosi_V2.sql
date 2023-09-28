582fd546636e2925c50b8806bf0adfcae249f362
Kevin Rognon <rognonk@gmail.com>
2023-09-28 18:58:34 +0200

    Mise à jour

--- tifosi_V2.sql
+++ tifosi_V2.sql
@@ -10,7 +10,7 @@ USE Tifosi_V2;
 
 CREATE TABLE IF NOT EXISTS `client`
 (
-	`id_client` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`id_client` INT PRIMARY KEY AUTO_INCREMENT,
 	`nom_client` VARCHAR(45) NOT NULL,
 	`age` INT NOT NULL,
 	`cp_client` INT NOT NULL
@@ -18,14 +18,14 @@ CREATE TABLE IF NOT EXISTS `client`
 
 CREATE TABLE IF NOT EXISTS `focaccia`
 (
-	`id_focaccia` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`id_focaccia` INT PRIMARY KEY AUTO_INCREMENT,
 	`nom_focaccia` VARCHAR(45) NOT NULL,
 	`prix_focaccia` FLOAT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `menu`
 (
-	`id_menu` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`id_menu` INT PRIMARY KEY AUTO_INCREMENT,
 	`nom_menu` VARCHAR(45) NOT NULL,
 	`prix_menu` FLOAT NOT NULL,
 	FOREIGN KEY (id_menu) REFERENCES focaccia(id_focaccia)
@@ -33,20 +33,20 @@ CREATE TABLE IF NOT EXISTS `menu`
 
 CREATE TABLE IF NOT EXISTS `ingredient`
 (
-	`id_ingredient` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`id_ingredient` INT PRIMARY KEY AUTO_INCREMENT,
 	`nom_ingredient` VARCHAR(45),
 	FOREIGN KEY (id_ingredient) REFERENCES focaccia(id_focaccia)
 );
 
 CREATE TABLE IF NOT EXISTS `marque`
 (
-	`id_marque` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`id_marque` INT PRIMARY KEY AUTO_INCREMENT,
 	`nom_marque` VARCHAR(45)
 );
 
 CREATE TABLE IF NOT EXISTS `boisson`
 (
-	`id_boisson` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`id_boisson` INT PRIMARY KEY AUTO_INCREMENT,
 	`nom_boisson` VARCHAR(45) NOT NULL,
 	FOREIGN KEY (id_boisson) REFERENCES menu(id_menu),
 	FOREIGN KEY (id_boisson) REFERENCES marque(id_marque)
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `boisson`
 
 CREATE TABLE IF NOT EXISTS `achete`
 (
-    `id_achat` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `id_achat` INT PRIMARY KEY AUTO_INCREMENT,
     `id_client` INT NOT NULL,
     `id_focaccia` INT NOT NULL,
     `jour` DATE NOT NULL,
@@ -65,9 +65,19 @@ CREATE TABLE IF NOT EXISTS `achete`
 
 CREATE TABLE IF NOT EXISTS `paye`
 (
-	`id_paye` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`id_paye` INT PRIMARY KEY AUTO_INCREMENT,
 	`id_client` INT NOT NULL,
 	`id_menu` INT NOT NULL,
 	FOREIGN KEY (id_client) REFERENCES client(id_client),
 	FOREIGN KEY (id_menu) REFERENCES menu(id_menu)
-)
+);
+
+
+-- INSERTIONS
+
+INSERT INTO focaccia (`nom_focaccia`, `prix_focaccia`) VALUES ('Mozaccia', 9.80), ('Gorgonzollaccia', 10.80), ('Raclaccia', 8.90), ('Emmentalaccia', 9.80), ('Tradizione', 8.90), ('Hawaienne', 11.20), ('Américaine', 10.80), ('Paysanne', 12.80), ('Emmentalaccia', 9.80);
+
+INSERT INTO ingredient (`nom_ingredient`) VALUES ('Ail'), ('Ananas'), ('Artichaut'), ('Bacon'), ('Base Tomate'), ('Base crème'), ('Champignon'), ('Chevre'), ('Cresson'), ('Emmental'), ('Gorgonzola'), ('Jambon cuit'), ('Jambon fumé'), ('Oeuf'), ('Oignon'), ('Olive noire'), ('Olive verte'), ('Parmesan'), ('Piment'), ('Poivre'), ('Pomme de terre'), ('Raclette'), ('Salami'), ('Tomate cerise');
+
+
+INSERT INTO marque (`nom_marque`) VALUES ('Coca-Cola'), ('Cristalline'), ('Monster'), ('Pepsico');
