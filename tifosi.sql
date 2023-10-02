e38af56a632d2fec627fa5df2f9c1f40e685e44f
Kevin Rognon <rognonk@gmail.com>
2023-10-02 15:10:50 +0200

    Mise à jour du script de création de tables.

--- tifosi.sql
+++ tifosi.sql
@@ -34,8 +34,7 @@ CREATE TABLE IF NOT EXISTS `menu`
 CREATE TABLE IF NOT EXISTS `ingredient`
 (
     `id_ingredient` INT PRIMARY KEY AUTO_INCREMENT,
-    `nom_ingredient` VARCHAR(45),
-    FOREIGN KEY (`id_ingredient`) REFERENCES focaccia(`id_focaccia`)
+    `nom_ingredient` VARCHAR(45)
 );
 
 CREATE TABLE IF NOT EXISTS `marque`
@@ -48,14 +47,12 @@ CREATE TABLE IF NOT EXISTS `boisson`
 (
     `id_boisson` INT PRIMARY KEY AUTO_INCREMENT,
     `nom_boisson` VARCHAR(45) NOT NULL,
-    FOREIGN KEY (`id_boisson`) REFERENCES marque(`id_marque`),
-    FOREIGN KEY (`id_boisson`) REFERENCES menu(`id_menu`)
+    FOREIGN KEY (`id_boisson`) REFERENCES marque(`id_marque`)
 );
 
 
 CREATE TABLE IF NOT EXISTS `achete`
 (
-    `id_achat` INT PRIMARY KEY AUTO_INCREMENT,
     `id_client` INT NOT NULL,
     `id_focaccia` INT NOT NULL,
     `jour` DATE NOT NULL,
@@ -65,10 +62,25 @@ CREATE TABLE IF NOT EXISTS `achete`
 
 CREATE TABLE IF NOT EXISTS `paye`
 (
-    `id_client` INT,
-    `id_menu` INT,
+    `id_client` INT NOT NULL,
+    `id_menu` INT NOT NULL,
     `jour` DATE NOT NULL,
-    PRIMARY KEY (`id_client`, `id_menu`),
     FOREIGN KEY (`id_client`) REFERENCES client(`id_client`),
     FOREIGN KEY (`id_menu`) REFERENCES menu(`id_menu`)
+);
+
+CREATE TABLE IF NOT EXISTS `comprend`
+(
+    `id_focaccia` INT NOT NULL,
+    `id_ingredient` INT NOT NULL,
+    FOREIGN KEY (`id_focaccia`) REFERENCES focaccia(`id_focaccia`),
+    FOREIGN KEY (`id_ingredient`) REFERENCES ingredient(`id_ingredient`)
+);
+
+CREATE TABLE IF NOT EXISTS `contient`
+(
+    `id_menu` INT NOT NULL,
+    `id_boisson` INT NOT NULL,
+    FOREIGN KEY (`id_menu`) REFERENCES menu(`id_menu`),
+    FOREIGN KEY (`id_boisson`) REFERENCES boisson(`id_boisson`)
 );
