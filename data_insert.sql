d56ef3a67b35379b141b622f20302ab69c54aaed
Kevin Rognon <rognonk@gmail.com>
2023-10-02 09:22:01 +0200

    Mise à jour des données de tests

--- data_insert.sql
+++ data_insert.sql
@@ -1,1 +1,10 @@
 
+USE tifosi;
+
+INSERT INTO `client` (`nom_client`, `age`, `cp_client`)
+VALUES
+    ("Kevin ROGNON", 25, 1),
+    ("Alice Dupont", 30, 2),
+    ("Jean Martin", 22, 3),
+    ("Sophie Lambert", 28, 4),
+    ("Pierre Durand", 35, 5);
