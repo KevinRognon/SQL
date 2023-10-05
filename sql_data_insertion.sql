3892e81ac723af95a49d20c0df0e42c1824a7386
Kevin Rognon <rognonk@gmail.com>
2023-10-05 14:47:01 +0200

    Mise à jour

--- sql_data_insertion.sql
+++ sql_data_insertion.sql
@@ -17,39 +17,52 @@ INSERT INTO `client` (`nom_client`, `age`, `cp_client`) VALUES
 -- Ajout des marques de boissons
 
 INSERT INTO `marque` (`nom_marque`) VALUES
-('Coca-cola'),
-('Cristalline'),
-('Monster'),
-('Pepsico');
+    ('Coca-cola'),
+    ('Cristalline'),
+    ('Monster'),
+    ('Pepsico');
 
 
--- Ajout des boissons
-
-INSERT INTO `boisson` (`nom_boisson`, `id_marque`) VALUES
-    ('Coca-cola zéro', 1),
-    ('Coca-cola original', 1),
-    ('Fanta citron', 1),
-    ('Fanta orange', 1),
-    ('Capri-sun', 1),
-    ('Pepsi', 4),
-    ('Pepsi Max Zéro', 4),
-    ('Lipton zéro citron', 4),
-    ('Lipton Peach', 4),
-    ('Monster energy ultra gold', 3),
-    ('Monster energy ultra blue', 3),
-    ('Eau de source', 2);
-
-
--- Ajout des focaccias
 
 INSERT INTO `focaccia` (`nom_focaccia`, `prix_focaccia`) VALUES
-('Mozaccia', 9.80),
-('Gorgonzollaccia', 10.80),
-('Raclaccia', 8.90),
-('Emmentalaccia', 9.80),
-('Tradizione', 8.90),
-('Hawaienne', 11.20),
-('Américaine', 10.80),
-('Paysanne', 12.80);
+    ('Mozaccia', 9.80),
+    ('Gorgonzollaccia', 10.80),
+    ('Raclaccia', 8.90),
+    ('Emmentalaccia', 9.80),
+    ('Tradizione', 8.90),
+    ('Hawaienne', 11.20),
+    ('Américaine', 10.80),
+    ('Paysanne', 12.80);
+
+
+
+-- Ajout des menus
+
+-- Création des menus pour chaque focaccia
+INSERT INTO `menu` (`nom_menu`, `prix_menu`, `id_focaccia`)
+VALUES
+    ('Menu Mozaccia', 12.50, 1),
+    ('Menu Gorgonzollaccia', 13.50, 2),
+    ('Menu Raclaccia', 11.60, 3),
+    ('Menu Emmentalaccia', 12.50, 4),
+    ('Menu Tradizione', 11.60, 5),
+    ('Menu Hawaienne', 14.00, 6),
+    ('Menu Américaine', 13.50, 7),
+    ('Menu Paysanne', 15.50, 8);
+
+
+-- INSERT INTO `boisson` (`nom_boisson`, `id_marque`) VALUES
+--     ('Coca-cola zéro', 1),
+--     ('Coca-cola original', 1),
+--     ('Fanta citron', 1),
+--     ('Fanta orange', 1),
+--     ('Capri-sun', 1),
+--     ('Pepsi', 4),
+--     ('Pepsi Max Zéro', 4),
+--     ('Lipton zéro citron', 4),
+--     ('Lipton Peach', 4),
+--     ('Monster energy ultra gold', 3),
+--     ('Monster energy ultra blue', 3),
+--     ('Eau de source', 2);
 
 
--- tifosi.sql
+++ tifosi.sql
@@ -1,12 +1,10 @@
--- CREATE USER IF NOT EXISTS 'tifosi'@'localhost' IDENTIFIED BY 'tifosi';
-
--- GRANT IF NOT EXISTS ALL PRIVILEGES ON Tifosi_V2.* TO 'tifosi'@'localhost';
-
---   FLUSH PRIVILEGES;
-
 CREATE DATABASE IF NOT EXISTS tifosi;
 
 USE tifosi;
+
+CREATE USER IF NOT EXISTS 'tifosi'@'localhost' IDENTIFIED BY 'tifosi';
+GRANT ALL PRIVILEGES ON tifosi.* TO 'tifosi'@'localhost';
+FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS `client`
 (
@@ -14,75 +12,78 @@ CREATE TABLE IF NOT EXISTS `client`
     `nom_client` VARCHAR(45) NOT NULL,
     `age` INT NOT NULL,
     `cp_client` INT NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS `focaccia`
 (
     `id_focaccia` INT PRIMARY KEY AUTO_INCREMENT,
     `nom_focaccia` VARCHAR(45) NOT NULL,
     `prix_focaccia` FLOAT NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS `menu`
 (
     `id_menu` INT PRIMARY KEY AUTO_INCREMENT,
     `nom_menu` VARCHAR(45) NOT NULL,
     `prix_menu` FLOAT NOT NULL,
-    FOREIGN KEY (`id_menu`) REFERENCES focaccia(`id_focaccia`)
-);
+    `id_focaccia` INT NOT NULL,
+    FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia`(`id_focaccia`)
+    );
 
 CREATE TABLE IF NOT EXISTS `ingredient`
 (
     `id_ingredient` INT PRIMARY KEY AUTO_INCREMENT,
     `nom_ingredient` VARCHAR(45),
-    FOREIGN KEY (`id_ingredient`) REFERENCES focaccia(`id_focaccia`)
-);
+    `id_focaccia` INT NOT NULL,
+    FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia`(`id_focaccia`)
+    );
 
 CREATE TABLE IF NOT EXISTS `marque`
 (
     `id_marque` INT PRIMARY KEY AUTO_INCREMENT,
     `nom_marque` VARCHAR(45)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS `boisson`
 (
     `id_boisson` INT PRIMARY KEY AUTO_INCREMENT,
     `nom_boisson` VARCHAR(45) NOT NULL,
-    FOREIGN KEY (`id_boisson`) REFERENCES marque(`id_marque`),
-    FOREIGN KEY (`id_boisson`) REFERENCES menu(`id_menu`)
-);
-
+    `id_marque` INT NOT NULL,
+    `id_menu` INT NOT NULL,
+    FOREIGN KEY (`id_marque`) REFERENCES `marque`(`id_marque`),
+    FOREIGN KEY (`id_menu`) REFERENCES `menu`(`id_menu`)
+    );
 
 CREATE TABLE IF NOT EXISTS `achete`
 (
     `id_client` INT NOT NULL,
     `id_focaccia` INT NOT NULL,
     `jour` DATE NOT NULL,
-    FOREIGN KEY (`id_client`) REFERENCES client(`id_client`),
-    FOREIGN KEY (`id_focaccia`) REFERENCES focaccia(`id_focaccia`)
-);
+    FOREIGN KEY (`id_client`) REFERENCES `client`(`id_client`),
+    FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia`(`id_focaccia`)
+    );
 
 CREATE TABLE IF NOT EXISTS `paye`
 (
     `id_client` INT NOT NULL,
     `id_menu` INT NOT NULL,
     `jour` DATE NOT NULL,
-    FOREIGN KEY (`id_client`) REFERENCES client(`id_client`),
-    FOREIGN KEY (`id_menu`) REFERENCES menu(`id_menu`)
-);
+    FOREIGN KEY (`id_client`) REFERENCES `client`(`id_client`),
+    FOREIGN KEY (`id_menu`) REFERENCES `menu`(`id_menu`)
+    );
 
 CREATE TABLE IF NOT EXISTS `comprend`
 (
     `id_focaccia` INT NOT NULL,
     `id_ingredient` INT NOT NULL,
-    FOREIGN KEY (`id_focaccia`) REFERENCES focaccia(`id_focaccia`),
-    FOREIGN KEY (`id_ingredient`) REFERENCES ingredient(`id_ingredient`)
-);
+    FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia`(`id_focaccia`),
+    FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient`(`id_ingredient`)
+    );
 
 CREATE TABLE IF NOT EXISTS `contient`
 (
     `id_menu` INT NOT NULL,
     `id_boisson` INT NOT NULL,
-    FOREIGN KEY (`id_menu`) REFERENCES menu(`id_menu`),
-    FOREIGN KEY (`id_boisson`) REFERENCES boisson(`id_boisson`)
-);
+    FOREIGN KEY (`id_menu`) REFERENCES `menu`(`id_menu`),
+    FOREIGN KEY (`id_boisson`) REFERENCES `boisson`(`id_boisson`)
+    );
